d70014bc23c9e3374c894001efe89c454368dd6d
Paul Atlan <paulatlan@fastmail.fm>
2019-08-08 05:41:52 +0200

    .
    recommitting

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -10,3 +10,10 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+# various pandoc pipes for on the fly conversion of the clipboard
+#
+alias prtf="pbpaste | pandoc -f markdown -t rtf | pbcopy -Prefer rtf; echo 'Converted clipboard to rtf'"
+
+# use Typora from the command line
+alias typora="open -a typora"
